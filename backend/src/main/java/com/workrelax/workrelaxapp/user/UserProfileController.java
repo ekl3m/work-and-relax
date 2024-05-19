f75437b85e269a503ec58dd6155482401b5e0759
ekl3m <eryk.klem03@gmail.com>
2024-05-19 20:12:31 +0200

    V0.1.1 : Added PUT request support for Users. It is also possible now to find users by userplan IDs.

--- backend/src/main/java/com/workrelax/workrelaxapp/user/UserProfileController.java
+++ backend/src/main/java/com/workrelax/workrelaxapp/user/UserProfileController.java
@@ -4,12 +4,15 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+
 
 @RestController
 @RequestMapping(path = "api/v1/userprofile")
@@ -27,12 +30,23 @@ public class UserProfileController {
     }
 
     @PostMapping
-    public void registerNewUser(@RequestBody UserProfile user) {
+    public void registerUser(@RequestBody UserProfile user) {
         userService.addNewUser(user);
     }
 
     @DeleteMapping(path = "{userProfileId}")
     public void deleteUser(@PathVariable("userProfileId") Long userId) {
         userService.deleteUser(userId);
+    }
+
+    @PutMapping(path = "{userProfileId}")
+    public void updateUser(@PathVariable("userProfileId") Long id, 
+        @RequestParam(required = false) String name, 
+        @RequestParam(required = false) String surname,
+        @RequestParam(required = false) String email,
+        @RequestParam(required = false) String password,
+        @RequestParam(required = false) List<Long> friendlistIds,
+        @RequestParam(required = false) Long userplanId) {
+        userService.updateUser(id, name, surname, email, password, friendlistIds, userplanId);
     }
 }
--- backend/src/main/java/com/workrelax/workrelaxapp/user/UserProfileRepository.java
+++ backend/src/main/java/com/workrelax/workrelaxapp/user/UserProfileRepository.java
@@ -11,4 +11,7 @@ public interface UserProfileRepository extends JpaRepository<UserProfile, Long> 
 
     @Query("SELECT s FROM UserProfile u WHERE u.email = ?1")
     Optional<UserProfile> findUserByEmail(String email);
+
+    @Query("SELECT s FROM UserProfile u WHERE u.userplanid = ?1")
+    Optional<UserProfile> findUserByUserplan(Long userplanid);
 }
