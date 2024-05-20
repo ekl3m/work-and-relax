3b1f92fd16d5e5b9a77c8fa0fbe585ffd7775ecc
ekl3m <eryk.klem03@gmail.com>
2024-05-20 11:30:04 +0200

    V0.1.2 : Added basic xcode project for writing frontend

---
+++ frontend/WorkAndRelax/WorkAndRelax/WorkAndRelaxApp.swift
@@ -0,0 +1,32 @@
+//
+//  WorkAndRelaxApp.swift
+//  WorkAndRelax
+//
+//  Created by Eryk Klemencki on 20/05/2024.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct WorkAndRelaxApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
